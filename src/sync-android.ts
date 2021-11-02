cbd66b8b7df37c3052ff4af544bb3a0ff59ac25b
Ian Tearle <ian@iantearle.com>
2021-11-02 12:07:54 +0000

    Provide build number to Android

--- src/sync-android.ts
+++ src/sync-android.ts
@@ -1,39 +1,35 @@
-import { resolve } from "path";
-
-import to from "await-to-js";
-import chalk from "chalk";
-import readProps from "properties-reader";
-import { valid } from "semver";
-
-import { log, readPackage } from "./utils";
-
-const appPropertiesPath = resolve(process.cwd(), "android/app/app.properties");
-
-const generateVersionCode = (version: string) => {
-  const [major, minor, patch] = version.split(".");
-  return (parseInt(major) * 1000000) + (parseInt(minor) * 1000) + parseInt(patch);
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
 };
-
-export const syncAndroid = async () => {
-  const { version } = readPackage(resolve(process.cwd(), "package.json"));
-
-  if (!valid(version)) {
-    log(chalk`  {red Invalid version: "${version}". Nothing to do.}`);
-    process.exit();
-  }
-
-  const appProps = readProps(appPropertiesPath);
-  const newVersionCode = generateVersionCode(version);
-
-  appProps.set("versionCode", newVersionCode);
-  appProps.set("versionName", version);
-
-  const [err] = await to(appProps.save(appPropertiesPath));
-
-  if (err) {
-    log(chalk`  {red Failed to sync version ${err.message}}`);
-    process.exit();
-  }
-
-  log(chalk`{green ✔} Sync version ${version} for android.`);
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.syncAndroid = void 0;
+const path_1 = require("path");
+const await_to_js_1 = __importDefault(require("await-to-js"));
+const chalk_1 = __importDefault(require("chalk"));
+const properties_reader_1 = __importDefault(require("properties-reader"));
+const semver_1 = require("semver");
+const utils_1 = require("./utils");
+const appPropertiesPath = path_1.resolve(process.cwd(), "android/app/app.properties");
+const generateVersionCode = (version) => {
+    const [major, minor, patch] = version.split(".");
+    return (parseInt(major) * 1000000) + (parseInt(minor) * 1000) + parseInt(patch);
 };
+const syncAndroid = async () => {
+    const { version, build } = utils_1.readPackage(path_1.resolve(process.cwd(), "package.json"));
+    if (!semver_1.valid(version)) {
+        utils_1.log(chalk_1.default `  {red Invalid version: "${version}". Nothing to do.}`);
+        process.exit();
+    }
+    const appProps = properties_reader_1.default(appPropertiesPath);
+    const newVersionCode = generateVersionCode(version);
+    appProps.set("versionCode", build);
+    appProps.set("versionName", version);
+    const [err] = await await_to_js_1.default(appProps.save(appPropertiesPath));
+    if (err) {
+        utils_1.log(chalk_1.default `  {red Failed to sync version ${err.message}}`);
+        process.exit();
+    }
+    utils_1.log(chalk_1.default `{green ✔} Sync version ${version} for android.`);
+};
+exports.syncAndroid = syncAndroid;
